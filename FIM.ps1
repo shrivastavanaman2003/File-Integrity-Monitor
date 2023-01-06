2fe93c66d2ea84fd7798d79b7423f6cd8cb2de26
Naman Shrivastava <78251454+shrivastavanaman2003@users.noreply.github.com>
2023-01-06 23:30:55 +0530

    Add files via upload

---
+++ FIM.ps1
@@ -0,0 +1,16 @@
+﻿Write-Host ""
+Write-Host "What would you like to do?"
+Write-Host "A) Collect new Baseline?"
+Write-Host "B) Begin monitoring fiels with saved Baseline?"
+Write-Host ""
+
+$response = Read-Host -Prompt "Please enter 'A' or 'B' "
+
+if($response -eq "A".ToUpper()){
+    #Calculate the hash from the target files and store in baseline.txt
+    Write-Host "Calculate Hashes, make new baseline.txt" -ForegroundColor Cyan
+}
+elseif($response -eq "B".ToUpper()){
+    #Begin monitoring files with saved baseline
+    Write-Host "Read existing baseline.txt, start monitoring files." -ForegroundColor Yellow
+}
